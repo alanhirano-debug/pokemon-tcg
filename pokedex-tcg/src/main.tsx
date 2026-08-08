44341060301b90351af30ca2fc2869162a971df5
alanhirano-debug <alanhirano@gmail.com>
2026-08-08 19:42:27 -0400

    Update BrowserRouter to use basename from env

--- pokedex-tcg/src/main.tsx
+++ pokedex-tcg/src/main.tsx
@@ -6,8 +6,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
+      <BrowserRouter basename={import.meta.env.BASE_URL}>    
+        <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
